6017ca7dabee64324024896050a4c85b8e9e871f
CHRISTOPHER_WEHE <chriswehe@gmail.com>
2019-10-01 10:01:09 -0400

    App.js merged

--- src/App.js
+++ src/App.js
@@ -34,26 +34,30 @@ class App extends Component {
      return(
     <div className="App">
        <NASAHeader >
-       <ul>
-        <Link to ="/">
-        <li>Home</li>
-        </Link>
-        <Link to="/PhotoOfTheDay">
-        <li>Photo of the Day Gallery</li>
-        </Link>
-        <Link to="/Mars">
-        <li>Mars Rover Photo Gallery</li>
-        </Link>
-        <Link to="/Amateur">
-        <li>Amateur Astronomer Gallery</li>
-        </Link>
-      </ul>  
-       </NASAHeader>
-      <NASAFooter />
-      <Route path ="/" exact render = {props => <Home  {...props}/>}/>
-      <Route path ="/PhotoOfTheDay" exact render = {props => <PhotoOfDay  {...props}/>}/>
-      <Route path ="/Mars" exact render = {props => <Mars  {...props}/>}/>
-      <Route path ="/Amateur" exact render = {props => <Amateur  {...props}/>}/>
+        <ul>
+          <Link to ="/">
+          <li>Home</li>
+          </Link>
+          <Link to="/PhotoOfTheDay">
+          <li>Photo of the Day Gallery</li>
+          </Link>
+          <Link to="/Mars">
+          <li>Mars Rover Photo Gallery</li>
+          </Link>
+          <Link to="/Amateur">
+          <li>Amateur Astronomer Gallery</li>
+          </Link>
+        </ul>  
+      </NASAHeader>
+      <main>
+        <Route path ="/" exact render = {props => <Home  {...props}/>}/>
+        <Route path ="/PhotoOfTheDay" exact render = {props => <PhotoOfDay  {...props}/>}/>
+        <Route path ="/Mars" exact render = {props => <Mars  {...props}/>}/>
+        <Route path ="/Amateur" exact render = {props => <Amateur  {...props}/>}/>
+      </main>
+      <NASAFooter/>
+
+      
 
 
 
