44830e6862d94b24aabac8b0dd3a01910a632565
Will McAllister <willmca32@gmail.com>
2019-10-01 08:04:10 -0400

    animation lessened, third opion added on home page

--- src/Home.js
+++ src/Home.js
@@ -12,10 +12,13 @@ class Home extends Component {
          <h1 className="title">Photos from the Great Beyond</h1>
       </div>
       <div className="pod">
-        <h1 className="podWords">Photo of the Day</h1>
+        <h1 className="podWords">Photo of the Day Gallery</h1>
       </div>
       <div className="mars">
-        <h1 className="marsWords">Mars Rover Photos</h1>
+        <h1 className="marsWords">Mars Rover Photo Gallery</h1>
+      </div>
+      <div className="amateur">
+        <h1 className="amateurWords">Amateur Astronomer Gallery</h1>
       </div>
       </div>
        )
