d822499a1ce6fcc6c71af8d827beb8703357658e
COnokala <C.Onokala@gmail.com>
2019-10-01 14:42:42 -0400

    modal trying

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from "react-router-dom"
-
-// modals code goes here
+import { render } from "react-dom";
+import PropTypes from "prop-types";
+import injectSheet from "react-jss";
+import styles from "./IndexStyles";
+import SimpleModalLauncher from "./SimpleModalLauncher";
 
 class Home extends Component {
     constructor(props){
@@ -13,19 +16,26 @@ class Home extends Component {
          <div className="FirstText">
          <h1 className="title">Photos from the Great Beyond</h1>
       </div>
-      <div className="pod">
-        <h1 className="podWords">Photo of the Day Gallery</h1>
+      <SimpleModalLauncher buttonLabel="Photo of the Day">
+      <div className="pod classes.textModal">
+        <h1 className="podWords">Photo of the Day</h1>
       </div>
+      </SimpleModalLauncher>
       <div className="mars">
-        <h1 className="marsWords">Mars Rover Photo Gallery</h1>
-      </div>
-      <div className="amateur">
-        <h1 className="amateurWords">Amateur Astronomer Gallery</h1>
+        <h1 className="marsWords">Mars Rover Photos</h1>
       </div>
       </div>
        )
-     }
-    
-  }
+      }
+    }
+
+    Home.propTypes = {
+      sheet: PropTypes.object,
+      classes: PropTypes.object
+    };
   
+const StyledApp = injectSheet(styles)(Home);
+
+render(<StyledApp />, document.getElementById("root"));
+
   export default Home;
